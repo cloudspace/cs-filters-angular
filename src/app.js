35eecc949a6cc3c5681fd19b05f2ec1a9a2806e4
Christopher McLean <havok2905@gmail.com>
2015-05-20 13:31:19 -0400

    using right providers

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ var angular   = require('angular');
 
 var app = angular.module('cs-filters', []);
 
-app.filter('bytes',      function() { return CSFilters.Measurements.Bytes; });
-app.filter('bandwidth',  function() { return CSFilters.Measurements.Bytes; });
-app.filter('kilo-case',  function() { return CSFilters.Measurements.Bytes; });
+app.filter('bytes',      function() { return CSFilters.Measurements.Bytes;     });
+app.filter('bandwidth',  function() { return CSFilters.Measurements.Bandwidth; });
+app.filter('kilocase',   function() { return CSFilters.Measurements.KiloCase;  });
 
-app.filter('start-from', function() { return CSFilters.Collections.Bytes;  });
+app.filter('start-from', function() { return CSFilters.Collections.StartFrom;  });
 
 module.exports = app;
